c902a59efac8112fed99c3325133f065ac2297c9
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-03 17:54:31 +0200

    Update program.py

---
+++ program.py
@@ -0,0 +1,10 @@
+# [“apple”, “banana”, “cherry”, “kiwi”, “mango”]
+#create a list of all the fruits with the letter ‘a’
+# inside them using list comprehension
+
+#some_list = [1,4,7,12,19,22, 23, 26].
+# create of list of string for even or odd: in this case
+# [‘1 is odd’, ‘4 is even’, ‘7 is odd’ …] using list comprehension
+#*etgar: list1 = [1,2] ; list2 = [4,5].
+# create a list of tuples [(1,4), (1,5), (2,4), (2, 50)]
+# using list comprehension
